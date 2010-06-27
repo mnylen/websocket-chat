388b10f2f0cae8f1d883a80928c693a73d4ebae3
mikko.nylen <mikko.nylen@gmail.com>
2010-06-27 16:02:32 +0300

    rewrote the client javascript

--- client/client.js
+++ client/client.js
@@ -1,61 +1,132 @@
-var chatHandler = function() {
+// Handles outputting the log
+logHandler = function() {
+  var chatLog = "#chat-log";
+  
+  var createMessageDiv = function(messageObject) {
+    return $("<div />").addClass("message")
+      .append($("<span />").addClass("sender").append(messageObject.nick))
+      .append(" ")
+      .append($("<span />").addClass("message").append(messageObject.message));
+  };
+  
+  var scrollToBottom = function() {
+    $(chatLog).scrollTop($(chatLog)[0].scrollHeight);
+  };
+  
   return {
-    addMessageToChatLog: function(nick, message) {
-      chatHandler.createMessageContainer(nick, message).appendTo($("#chat-log"));
-      chatHandler.autoScrollToBottom();
-    },
-    
-    createMessageContainer: function(nick, message) {
-      var $messageDiv = $("<div />").addClass("message");
-      $("<span />").addClass("sender").append(nick).appendTo($messageDiv);
-      $messageDiv.append(" ");
-      $("<span />").addClass("message").append(message).appendTo($messageDiv);
-      return $messageDiv;
-    },
+    appendMessage: function(messageObject) {
+      createMessageDiv(messageObject).appendTo($(chatLog));
+      scrollToBottom();
+    }
+  }
+}();
 
-    autoScrollToBottom: function() {
-      var scrollTop = $("#chat-log")[0].scrollHeight
-      $("#chat-log").scrollTop(scrollTop);
-    },
-    
-    clearAndFocusOnMessageInput: function() {
-      $input = $("#chat-controls input[name='message']");
-      $input.attr("value", "");
-      $input.focus();
-    },
-    
-    sendMessage: function(webSocket) {
-      var messageObject = {
-        "nick": $("input[name='nick']").attr('value'),
-        "message": $("input[name='message']").attr('value')
-      };
+// Handles user input
+inputHandler = function() {
+  var sendButton = "#chat-controls button[name='send']";
+  var messageInput = "#chat-controls input[name='message']";
+  var nickInput = "#chat-controls input[name='nick']";
+  
+  var clearMessageAndFocusIt = function() {
+    $(messageInput).attr("value", "");
+    $(messageInput).focus();
+  };
+  
+  // returns the user provided nickname and message as JavaScript object 
+  var messageObject = function() {
+    var obj = {
+      "nick" : $(nickInput).attr('value'),
+      "message" : $(messageInput).attr('value')
+    };
+      
+    if (obj.nick == "" || obj.message == "") {
+      return null;
+    } else {
+      return obj;
+    }
+  };
+  
+  var sendMessage = function() {
+    return chat.sendMessage(messageObject());
+  };
+   
+  return {    
+    init: function() {
+      $(sendButton).click(function(event) {
+        event.preventDefault();
+        if (sendMessage()) {
+          clearMessageAndFocusIt();
+        }
+      });
+      
+      $(messageInput).keypress(function(event) {
+        if (event.keyCode == '13') {
+          event.preventDefault();
+          if (sendMessage()) {
+            clearMessageAndFocusIt();
+          }
+        }
+      });
+    } 
+  };
+}();
 
-      if (messageObject.nick != "" && messageObject.message != "") {
-        webSocket.send(JSON.stringify(messageObject));
-        chatHandler.addMessageToChatLog(messageObject.nick, messageObject.message);
-        chatHandler.clearAndFocusOnMessageInput();
-      }
+
+// Handles communication between client and server
+commHandler = function() {
+  var webSocket = null;
+  
+  var messageReceived = function(evt) {
+    var messageObject = $.parseJSON(evt.data);
+    chat.messageReceived(messageObject);
+  };
+  
+  return {
+    init: function() {
+      webSocket = new WebSocket("ws://127.0.0.1:8000");
+      webSocket.onmessage = messageReceived; 
+    },
+    
+    serializeAndSend: function(messageObject) {
+      webSocket.send(JSON.stringify(messageObject));
     }
   };
 }();
 
-$(document).ready(function() {
-  var webSocket = new WebSocket("ws://127.0.0.1:8000");
-  webSocket.onmessage = function(evt) {
-    var obj = $.parseJSON(evt.data);
-    chatHandler.addMessageToChatLog(obj.nick, obj.message);
-  };
-
-  $("#chat-controls button[name='send']").click(function() {
-    chatHandler.sendMessage(webSocket);
-  });
-  
-  
-  $("#chat-controls input").keypress(function(event) {
-    if (event.keyCode == '13') {
-      event.preventDefault();
-      
-      chatHandler.sendMessage(webSocket);
+// The chat module
+chat = function() {
+  var validMessageObject = function(messageObject) {
+    if (messageObject.nick == "" || messageObject.message == "") {
+      return false;
+    } else {
+      return true;
     }
-  });
+  };
+  
+  return {
+    init: function() {
+      inputHandler.init();
+      commHandler.init();
+    },
+    
+    messageReceived: function(messageObject) {
+      if (validMessageObject(messageObject)) {
+        logHandler.appendMessage(messageObject);
+      }
+    },
+    
+    sendMessage: function(messageObject) {
+      if (validMessageObject(messageObject)) {
+        commHandler.serializeAndSend(messageObject);
+        logHandler.appendMessage(messageObject);
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+}();
+
+$(document).ready(function() {
+  chat.init();
 });
