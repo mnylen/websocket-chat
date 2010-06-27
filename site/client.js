dc1607a48a5cec593029c6ca46d4282f781713af
mikko.nylen <mikko.nylen@gmail.com>
2010-06-27 13:59:48 +0300

    now sending messages is supported

--- site/client.js
+++ site/client.js
@@ -17,4 +17,15 @@ $(document).ready(function() {
 		var obj = $.parseJSON(evt.data);
 		chatHandler.addMessageToChatLog(obj.nick, obj.message);
 	};
+	
+	$("#chat-controls button[name='send']").click(function() {
+		var messageObject = {
+			"nick": $("input[name='nick']").attr('value'),
+			"message": $("input[name='message']").attr('value')};
+		
+		if (messageObject.nick != "" && messageObject.message != "") {
+			webSocket.send(JSON.stringify(messageObject));
+			chatHandler.addMessageToChatLog(messageObject.nick, messageObject.message);
+		}
+	});
 });
